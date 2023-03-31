103ffe445799eeac392014d26b618bd20ba493e1
H <HTechHQ@users.noreply.github.com>
2023-03-31 22:01:27 +0200

    feat: add hot reload & job proof of concept

--- go.mod
+++ go.mod
@@ -1,3 +1,41 @@
 module github.com/go-arrower/skeleton
 
-go 1.19
+go 1.20
+
+require (
+	github.com/go-arrower/arrower v0.0.0
+	github.com/labstack/echo/v4 v4.10.2
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/oklog/ulid/v2 v2.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/vgarvardt/backoff v1.0.0 // indirect
+	github.com/vgarvardt/gue/v5 v5.1.1 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/metric v0.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
+
+replace github.com/go-arrower/arrower v0.0.0 => ../arrower
--- main.go
+++ main.go
@@ -1,3 +1,148 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"errors"
+	"html/template"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+
+	"github.com/go-arrower/arrower/jobs"
+	"github.com/go-arrower/arrower/postgres"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	ctx := context.Background()
+	pg, err := postgres.ConnectAndMigrate(ctx, postgres.Config{
+		User:       "arrower",
+		Password:   "secret",
+		Database:   "arrower",
+		Host:       "localhost",
+		Port:       5432, //nolint:gomnd
+		MaxConns:   10,   //nolint:gomnd
+		Migrations: postgres.ArrowerDefaultMigrations,
+	})
+
+	log.Println(err)
+
+	router := echo.New()
+	router.Use(middleware.Static("public"))
+	router.Use(injectMW)
+
+	queue, _ := jobs.NewGueJobs(pg.PGx)
+	_ = queue.RegisterWorker(func(ctx context.Context, j someJob) error {
+		if rand.Intn(100) > 10 { //nolint:gosec,gomnd
+			return errors.New("some error") //nolint:goerr113
+		}
+
+		return nil
+	})
+	_ = queue.StartWorkers()
+
+	router.GET("/", func(c echo.Context) error {
+		_ = queue.Enqueue(ctx, someJob{"Hallo job!"})
+
+		return c.Render(http.StatusOK, "hello", "World") //nolint:wrapcheck
+	})
+
+	t := &Template{}
+	router.Renderer = t
+
+	router.Logger.Fatal(router.Start(":8080"))
+}
+
+type someJob struct {
+	Val string
+}
+
+func injectMW(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if err := next(c); err != nil {
+			c.Error(err)
+		}
+
+		if strings.Contains(c.Response().Header().Get("Content-Type"), "text/html") {
+			_, _ = c.Response().Write([]byte(hotReloadJSCode))
+		}
+
+		return nil
+	}
+}
+
+type Template struct{}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	templates := template.Must(template.ParseGlob("public/views/*.html"))
+
+	return templates.ExecuteTemplate(w, name, data) //nolint:wrapcheck
+}
+
+//nolint:lll
+const hotReloadJSCode = `<!-- Code injected by hot-reload middleware -->
+<div id="arrower-status" style="position:absolute; bottom:0; right:0; display:flex; flex-direction:column; align-items:flex-end; margin:10px;">
+	<svg style="width:75px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fa4901" class="w-6 h-6">
+  		<path fill-rule="evenodd" d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z" clip-rule="evenodd" />
+	</svg>
+	<span>Arrower not active!<span>
+</div>
+
+<script>
+	function refreshCSS() {
+		var sheets = [].slice.call(document.getElementsByTagName("link"));
+		var head = document.getElementsByTagName("head")[0];
+		for (var i = 0; i < sheets.length; ++i) {
+			var elem = sheets[i];
+			head.removeChild(elem);
+			var rel = elem.rel;
+			if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+				var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+				elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+			}
+			head.appendChild(elem);
+		}
+	}
+
+    var loc = window.location;
+    var uri = 'ws:';
+
+    if (loc.protocol === 'https:') {
+        uri = 'wss:';
+    }
+    //uri += '//' + loc.host;
+    uri += loc.hostname +':3030/' + 'ws';
+
+    console.log("connect to:", uri) // => todo build uri that connects to arrower cli instead of developer's app
+    ws = new WebSocket(uri)
+
+    ws.onopen = function() {
+        console.log('Connected')
+		setTimeout(function() {document.getElementById('arrower-status').style.visibility='hidden'}, 200);
+    }
+
+    ws.onmessage = function(msg) {
+		console.log("RECEIVED RELOAD", msg.data);
+        if (msg.data === 'reload') {
+			window.location.reload();
+		}
+		else if (msg.data == 'refreshCSS') {
+			refreshCSS();
+		}
+    }
+
+    ws.onclose = msg => {
+        console.log("Client closed connection")
+
+		document.getElementById('arrower-status').style.visibility='visible'
+        // setTimeout(window.location.reload.bind(window.location), 400);
+    }
+
+    ws.onerror = error => {
+        console.log("Socket error: ", error)
+    }
+</script>
+`
