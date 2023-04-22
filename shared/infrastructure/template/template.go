b5ae5df7bc68f6c6537b3bb6b302f6d619d96d38
H <HTechHQ@users.noreply.github.com>
2023-04-22 11:24:38 +0200

    feat: add basic view loading prototype for shared views #2

---
+++ shared/infrastructure/template/template.go
@@ -0,0 +1,244 @@
+package template
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"html/template"
+	"io"
+	"io/fs"
+	"log"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	ErrLoadFailed   = errors.New("load renderer failed")
+	ErrInvalidFS    = fmt.Errorf("%w: invalid fs", ErrLoadFailed)
+	ErrRenderFailed = errors.New("rendering failed")
+)
+
+const separator = "=>"
+
+type Renderer struct {
+	rawLayouts map[string]string
+	rawPages   map[string]string
+	templates  map[string]*template.Template
+
+	components *template.Template
+
+	isContextRenderer bool // true, if the renderer became a Context renderer and is not shared anymore.
+	hotReload         bool
+}
+
+// NewRenderer take multiple FS or can Context views be added later?
+// It prepares a renderer for HTML web views.
+func NewRenderer(viewFS fs.FS, hotReload bool) (*Renderer, error) {
+	if viewFS == nil {
+		return nil, ErrInvalidFS
+	}
+
+	components, err := fs.Glob(viewFS, "components/*.html")
+	if err != nil {
+		return nil, fmt.Errorf("%w: could not get components from fs: %v", ErrInvalidFS, err)
+	}
+
+	componentTemplates := template.New("")
+
+	for _, c := range components {
+		file, err := readFile(viewFS, c)
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not read component file: %s: %v", ErrInvalidFS, file, err)
+		}
+
+		name := componentName(c)
+
+		_, err = componentTemplates.New(name).Parse(file)
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not parse component: %s: %v", ErrInvalidFS, file, err)
+		}
+	}
+
+	log.Println("loaded components", len(componentTemplates.Templates()), componentTemplates.DefinedTemplates())
+
+	pageTemplates := make(map[string]*template.Template)
+
+	pages, err := fs.Glob(viewFS, "pages/*.html")
+	if err != nil {
+		return nil, fmt.Errorf("%w: could not get pages from fs: %v", ErrInvalidFS, err)
+	}
+
+	rawPages := make(map[string]string)
+
+	for _, page := range pages {
+		file, err := readFile(viewFS, page)
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not read page file: %s: %v", ErrInvalidFS, file, err)
+		}
+
+		tmp, err := componentTemplates.Clone()
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not clone component templates: %v", ErrLoadFailed, err)
+		}
+
+		pn := pageName(page)
+		rawPages[pn] = file
+
+		pageTemplates[pn], err = tmp.New(pn).Parse(file)
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not parse page file: %s: %v", ErrInvalidFS, file, err)
+		}
+	}
+
+	log.Println("loaded pages", len(pageTemplates))
+
+	for _, p := range pageTemplates {
+		log.Println("page:", p.Name(), p.DefinedTemplates())
+	}
+
+	layouts, err := fs.Glob(viewFS, "*.html")
+	if err != nil {
+		return nil, fmt.Errorf("%w: could not get layouts from fs: %v", ErrInvalidFS, err)
+	}
+
+	rawLayouts := make(map[string]string)
+
+	for _, l := range layouts {
+		file, err := readFile(viewFS, l)
+		if err != nil {
+			return nil, fmt.Errorf("%w: could not read layout file: %s: %v", ErrInvalidFS, file, err)
+		}
+
+		ln := layoutName(l)
+		rawLayouts[ln] = file
+	}
+
+	log.Println("layouts", layouts)
+
+	return &Renderer{
+		rawLayouts:        rawLayouts,
+		rawPages:          rawPages,
+		isContextRenderer: false,
+		components:        componentTemplates,
+		templates:         pageTemplates,
+		hotReload:         hotReload,
+	}, nil
+}
+
+func componentName(componentName string) string {
+	name := strings.TrimPrefix(componentName, "components/")
+	name = strings.TrimSuffix(name, ".html")
+
+	return name
+}
+
+func layoutName(layoutName string) string {
+	name := strings.TrimSuffix(layoutName, ".layout.html")
+
+	return name
+}
+
+func pageName(pageName string) string {
+	name := strings.TrimPrefix(pageName, "pages/")
+	name = strings.TrimSuffix(name, ".page.html")
+
+	return name
+}
+
+func readFile(sfs fs.FS, name string) (string, error) {
+	file, err := sfs.Open(name)
+	if err != nil {
+		return "", fmt.Errorf("%v", err) //nolint:errorlint,goerr113 // do not expose err to arrower api
+	}
+
+	var buf bytes.Buffer
+
+	_, err = io.Copy(&buf, file)
+	if err != nil {
+		return "", fmt.Errorf("could not read: %v", err) //nolint:errorlint,goerr113 // do not expose err to arrower api
+	}
+
+	return buf.String(), nil
+}
+
+func NewEMails(fs fs.FS) {}
+
+func (t *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	layout, page := parseLayoutAndPage(name)
+
+	cleanedName := layout + "=>" + page
+	if layout == "" {
+		cleanedName = page
+	}
+
+	log.Println("Render for template", name, "cleanedName", cleanedName)
+
+	templ, found := t.templates[name]
+	if !found {
+		log.Println("template does not exist", name)
+
+		log.Println("layout:", layout, "page:", page)
+
+		newTemplate, err := t.components.Clone()
+		if err != nil {
+			return fmt.Errorf("%w: could not clone: %v", ErrRenderFailed, err)
+		}
+
+		log.Println("newTemplate details:", newTemplate.Name(), newTemplate.DefinedTemplates())
+
+		_, err = newTemplate.New(cleanedName).Parse(t.rawLayouts[layout])
+		if err != nil {
+			return fmt.Errorf("%w: could not parse layout: %v", ErrRenderFailed, err)
+		}
+
+		log.Println("newTemplate details:", newTemplate.Name(), newTemplate.DefinedTemplates())
+
+		_, err = newTemplate.New("content").Parse(t.rawPages[page])
+		if err != nil {
+			return fmt.Errorf("%w: could not parse page: %v", ErrRenderFailed, err)
+		}
+
+		t.templates[cleanedName] = newTemplate
+		templ = newTemplate // "found" the template
+
+		log.Println("newTemplate added:", newTemplate.Name(), newTemplate.DefinedTemplates())
+	}
+
+	log.Println("found template", templ.Name(), templ.DefinedTemplates())
+
+	err := templ.ExecuteTemplate(w, cleanedName, data)
+	if err != nil {
+		return fmt.Errorf("%w: could not execute template: %v", ErrRenderFailed, err)
+	}
+
+	return nil
+}
+
+// parseLayoutAndPage accepts:
+// - page
+// - layout=>page
+// - layout=>sub-layout=>page
+// and returns the layout (composed if with sub-layout) and the page.
+func parseLayoutAndPage(name string) (string, string) {
+	const maxCompositionSegments = 3 // how many segments after separated by the separator
+
+	elem := strings.Split(name, separator)
+
+	length := len(elem)
+
+	if length > maxCompositionSegments { // invalid pattern
+		return "", ""
+	}
+
+	if length == 1 {
+		return "", strings.TrimSpace(elem[0])
+	}
+
+	l := strings.TrimSpace(elem[0])
+	if length == maxCompositionSegments {
+		l = l + separator + strings.TrimSpace(elem[1])
+	}
+
+	return l, strings.TrimSpace(elem[length-1])
+}
---
+++ shared/infrastructure/template/template_test.go
@@ -0,0 +1,256 @@
+// Use white box testing, to make it easier to assert on the inner workings of partially loaded and cached templates.
+// if a white box test case fails, consider just deleting it over fixing it, to prevent coupling to the implementation.
+//
+//nolint:testpackage
+package template
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/go-arrower/skeleton/shared/infrastructure/template/testdata"
+	"github.com/go-arrower/skeleton/shared/interfaces/web/views"
+)
+
+func TestNewRenderer(t *testing.T) {
+	t.Parallel()
+
+	t.Run("construct renderer", func(t *testing.T) {
+		t.Parallel()
+
+		r, err := NewRenderer(views.SharedViews, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, r)
+	})
+
+	t.Run(" fail on missing files", func(t *testing.T) {
+		t.Parallel()
+
+		r, err := NewRenderer(nil, false)
+		assert.Error(t, err)
+		assert.Nil(t, r)
+	})
+
+	// white box test, if it fails feel free to delete it
+	t.Run("initialise raw renderer", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.SimpleFiles, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		// assert component templates from testdata.SimpleFiles is loaded
+		// contains always an empty component, so expected 2 +1 => 3
+		assert.Len(t, renderer.components.Templates(), 3)
+
+		// assert pages are loaded
+		assert.Len(t, renderer.templates, 2)
+		// if the file is called p0.page.html, the template is called p0
+		assert.NotEmpty(t, renderer.templates["p0"])
+		assert.NotEmpty(t, renderer.templates["p1"])
+		assert.Empty(t, renderer.templates["non-existent"])
+
+		// assert each page has itself and all components loaded as a template
+		for _, page := range renderer.templates {
+			assert.Len(t, page.Templates(), 4)
+		}
+
+		// assert template is cached
+		// if the file is called global.layout.html, the template is called global
+		assert.Len(t, renderer.rawLayouts, 1)
+		assert.NotEmpty(t, renderer.rawLayouts["global"])
+		assert.Empty(t, renderer.rawLayouts["non-existent"])
+	})
+
+	/*
+		additional test cases:
+		- folder structure without components
+		- folder structure without pages
+		- folder structure without layouts
+	*/
+}
+
+func TestRenderer_Render(t *testing.T) {
+	t.Parallel()
+
+	t.Run("render shared pages without layout", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.SimpleFiles, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		buf := &bytes.Buffer{}
+		err = renderer.Render(buf, "p0", nil, nil)
+		assert.NoError(t, err)
+
+		assert.Contains(t, buf.String(), testdata.P0Content)
+	})
+
+	t.Run("render shared pages with components", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.LayoutsPagesAndComponents, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		buf := &bytes.Buffer{}
+		err = renderer.Render(buf, "p0", nil, nil)
+		assert.NoError(t, err)
+
+		assert.Contains(t, buf.String(), testdata.P0Content)
+		assert.Contains(t, buf.String(), testdata.C0Content)
+		assert.NotContains(t, buf.String(), testdata.C1Content)
+	})
+
+	t.Run("render shared page with different layouts", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.LayoutsPagesAndComponents, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		buf := &bytes.Buffer{}
+		err = renderer.Render(buf, "global=>p0", nil, nil)
+		assert.NoError(t, err)
+
+		assert.Contains(t, buf.String(), testdata.LContent)
+		assert.Contains(t, buf.String(), testdata.P0Content)
+		assert.Contains(t, buf.String(), testdata.C0Content)
+		assert.NotContains(t, buf.String(), testdata.C1Content)
+
+		buf.Reset()
+		err = renderer.Render(buf, "other=>p0", nil, nil)
+		assert.NoError(t, err)
+
+		assert.Contains(t, buf.String(), "other")
+		assert.NotContains(t, buf.String(), testdata.LContent)
+		assert.Contains(t, buf.String(), testdata.P0Content)
+		assert.Contains(t, buf.String(), testdata.C0Content)
+		assert.NotContains(t, buf.String(), testdata.C1Content)
+	})
+
+	// white box test, if it fails feel free to delete it
+	t.Run("render multiple pages and increase template cache", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.LayoutsPagesAndComponents, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		// assert only the pure pages are loaded, because others are not cached yet
+		assert.Len(t, renderer.templates, 2)
+
+		buf := &bytes.Buffer{}
+		err = renderer.Render(buf, "global=>p0", nil, nil)
+		assert.NoError(t, err)
+		assert.Len(t, renderer.templates, 3) // previous templates + global=>p0
+
+		assert.Contains(t, buf.String(), testdata.LContent)
+		assert.Contains(t, buf.String(), testdata.P0Content)
+		assert.Contains(t, buf.String(), testdata.C0Content)
+		assert.NotContains(t, buf.String(), testdata.C1Content)
+
+		buf.Reset()
+		err = renderer.Render(buf, "global=>p1", nil, nil)
+		assert.NoError(t, err)
+		assert.Len(t, renderer.templates, 4) // previous templates + global=>p1
+
+		assert.Contains(t, buf.String(), testdata.LContent)
+		assert.Contains(t, buf.String(), testdata.P1Content)
+		assert.NotContains(t, buf.String(), testdata.C0Content)
+		assert.NotContains(t, buf.String(), testdata.C1Content)
+
+		buf.Reset()
+		err = renderer.Render(buf, "global=>p1", nil, nil)
+		assert.NoError(t, err)
+		assert.Len(t, renderer.templates, 4) // template is cached already, so no change
+	})
+
+	t.Run("render component", func(t *testing.T) {
+		t.Parallel()
+
+		renderer, err := NewRenderer(testdata.SimpleFiles, false)
+		assert.NoError(t, err)
+		assert.NotNil(t, renderer)
+
+		buf := &bytes.Buffer{}
+		err = renderer.Render(buf, "c0.component", nil, nil)
+		assert.NoError(t, err)
+
+		assert.Contains(t, buf.String(), testdata.C0Content)
+	})
+
+	/*
+		test cases
+		- Test parallel rendering, to prevent race conditions
+		- use layout name that does not exist
+		- hotreload from local fs
+	*/
+}
+
+// white box test, if it fails feel free to delete it.
+func TestParseLayoutAndPage(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		testName       string
+		name           string
+		expectedLayout string
+		expectedPage   string
+	}{
+		{
+			"empty",
+			"",
+			"",
+			"",
+		},
+		{
+			"just page",
+			"p0",
+			"",
+			"p0",
+		},
+		{
+			"layout and page",
+			"l=>p",
+			"l",
+			"p",
+		},
+		{
+			"trim whitespaces",
+			" l => p ",
+			"l",
+			"p",
+		},
+		{
+			"layout, sub-layout, and page",
+			"l=>s=>p",
+			"l=>s",
+			"p",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.testName, func(t *testing.T) {
+			t.Parallel()
+
+			l, p := parseLayoutAndPage(tt.name)
+			assert.Equal(t, tt.expectedLayout, l)
+			assert.Equal(t, tt.expectedPage, p)
+		})
+	}
+}
+
+/*
+Additional API
+- AddContext to add more views that are not global but context specific
+	- call twice, and it should fail, as it is already a context renderer
+	- keep original renderer unchanged, so it can be continued to used as is and for other contexts as well
+	- call with global layout, sub-layout, and page
+- E-Mail renderer instead of web renderer
+*/
