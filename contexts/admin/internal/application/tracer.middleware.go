39b41d4fc31f1113a7cc4a5ce6190c1815af531c
H <HTechHQ@users.noreply.github.com>
2023-07-12 11:29:34 +0900

    feat: add unary trace middle ware #8

--- contexts/admin/internal/application/tracer.middleware.go
+++ contexts/admin/internal/application/tracer.middleware.go
@@ -28,3 +28,24 @@ func Traced[in, out any, F DecoratorFunc[in, out]](traceProvider trace.TracerPro
 		return result, err
 	}
 }
+
+// TracedU see Traced.
+func TracedU[in any, F DecoratorFuncUnary[in]](traceProvider trace.TracerProvider, next F) F { //nolint:ireturn
+	tracer := traceProvider.Tracer("arrower.application") // trace.WithInstrumentationVersion("0.0.0"),
+
+	return func(ctx context.Context, in in) error {
+		cmdName := commandName(in)
+
+		newCtx, span := tracer.Start(ctx, "usecase",
+			trace.WithAttributes(attribute.String("command", cmdName)),
+		)
+		defer span.End()
+
+		err := next(newCtx, in)
+		if err != nil {
+			span.SetStatus(codes.Error, err.Error())
+		}
+
+		return err
+	}
+}
--- contexts/admin/internal/application/tracer.middleware_test.go
+++ contexts/admin/internal/application/tracer.middleware_test.go
@@ -30,3 +30,27 @@ func TestTraced(t *testing.T) {
 		_, _ = cmd(context.Background(), exampleCommand{})
 	})
 }
+
+func TestTracedU(t *testing.T) {
+	t.Parallel()
+
+	t.Run("successful command", func(t *testing.T) {
+		t.Parallel()
+
+		cmd := application.TracedU(newFakeTracer(t), func(context.Context, exampleCommand) error {
+			return nil
+		})
+
+		_ = cmd(context.Background(), exampleCommand{})
+	})
+
+	t.Run("failed command", func(t *testing.T) {
+		t.Parallel()
+
+		cmd := application.TracedU(newFakeTracer(t), func(context.Context, exampleCommand) error {
+			return errUseCaseFails
+		})
+
+		_ = cmd(context.Background(), exampleCommand{})
+	})
+}
