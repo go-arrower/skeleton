3063d891627d2fd5826d629d8993083609a40f07
H <HTechHQ@users.noreply.github.com>
2023-10-20 17:50:23 +0200

    refactor: move the job queues pie chart of pending jobs to rsjs style

---
+++ public/js/behaviors/pending-jobs-by-queue.js
@@ -0,0 +1,68 @@
+initAllPendingJobsPieCharts();
+document.addEventListener("htmx:load", updateAllPendingJobsPieCharts);
+document.addEventListener('htmx:beforeOnLoad', leave)
+
+
+function initAllPendingJobsPieCharts() {
+    document.querySelectorAll("[data-js-pending-jobs-by-queue]").forEach(elem => {
+        elem.setAttribute('chart-initialised', true);
+
+        queuesChart = echarts.init(elem);
+
+        queuesChart.showLoading();
+        queuesChart.setOption({
+            tooltip: {
+                trigger: 'item',
+                formatter: '<b>{b}</b>: {c} ({d}%)'
+            },
+            title: {
+                text: 'Pending Jobs per Queue',
+                right: '25%',
+            },
+            series: [
+                {
+                    type: 'pie',
+                    data: [],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ],
+            // roseType: 'area'
+        });
+
+        updatePendingJobsPieChart(queuesChart)
+    })
+}
+
+function updateAllPendingJobsPieCharts() {
+    document.querySelectorAll("[data-js-pending-jobs-by-queue]").forEach(elem => {
+        queuesChart = echarts.init(elem);
+        updatePendingJobsPieChart(queuesChart)
+    })
+}
+
+async function updatePendingJobsPieChart(queuesChart) {
+    const response = await fetch("/admin/jobs/data/pending");
+    const data = await response.json();
+
+    queuesChart.hideLoading();
+    queuesChart.setOption({
+        series: [
+            {
+                data: data
+            }
+        ]
+    });
+}
+
+function leave (e) {
+    if (e.detail.pathInfo.finalRequestPath !== "/admin/jobs") { // user leaves page
+        document.removeEventListener('htmx:beforeOnLoad',leave)
+        document.removeEventListener('htmx:load',updateAllPendingJobsPieCharts)
+    }
+}
