4b12af714305ce8998665e7180d3bc45cb3bd509
H <HTechHQ@users.noreply.github.com>
2024-02-10 11:59:57 +0100

    refactor: clean admin initialisation (partially)

--- contexts/admin/init/init.admin.go
+++ contexts/admin/init/init.admin.go
@@ -3,136 +3,92 @@ package init
 import (
 	"context"
 	"log/slog"
-	"net/http"
-	"sort"
 
-	models3 "github.com/go-arrower/arrower/alog/models"
+	alogmodels "github.com/go-arrower/arrower/alog/models"
 	"github.com/go-arrower/arrower/mw"
-	"github.com/labstack/echo/v4"
 
 	"github.com/go-arrower/skeleton/contexts/admin/internal/application"
+	"github.com/go-arrower/skeleton/contexts/admin/internal/domain/jobs"
 	"github.com/go-arrower/skeleton/contexts/admin/internal/interfaces/repository"
-	models2 "github.com/go-arrower/skeleton/contexts/admin/internal/interfaces/repository/models"
+	"github.com/go-arrower/skeleton/contexts/admin/internal/interfaces/repository/models"
 	"github.com/go-arrower/skeleton/contexts/admin/internal/interfaces/web"
 	"github.com/go-arrower/skeleton/shared/infrastructure"
 	web2 "github.com/go-arrower/skeleton/shared/interfaces/web"
 )
 
 func NewAdminContext(di *infrastructure.Container) (*AdminContext, error) {
-	di.AdminRouter.GET("/", func(c echo.Context) error {
-		return c.Redirect(http.StatusSeeOther, "/")
-	})
+	adminContext := &AdminContext{
+		Container: di,
 
-	di.AdminRouter.GET("/routes", func(c echo.Context) error {
-		routes := di.WebRouter.Routes()
+		jobRepository: repository.NewTracedJobsRepository(repository.NewPostgresJobsRepository(di.PGx)),
 
-		// sort routes by path and then by method
-		sort.Slice(routes, func(i, j int) bool {
-			if routes[i].Path < routes[j].Path {
-				return true
-			}
+		settingsController: web.NewSettingsController(di.AdminRouter),
+		logsController:     web.NewLogsController(di.Logger, di.Settings, alogmodels.New(di.PGx), di.AdminRouter.Group("/logs"), web2.NewDefaultPresenter(di.Settings)),
+	}
 
-			if routes[i].Path == routes[j].Path {
-				return routes[i].Method < routes[j].Method
-			}
-
-			return false
-		})
-
-		return c.Render(http.StatusOK, "admin.routes", echo.Map{
-			"Flashes": nil,
-			"Routes":  routes,
-		})
-	})
-
-	repo := repository.NewTracedJobsRepository(repository.NewPostgresJobsRepository(di.PGx))
-
+	jobsController := web.NewJobsController(di.Logger, adminContext.jobRepository, web2.NewDefaultPresenter(di.Settings), application.NewJobsApplication(di.PGx))
 	container := application.JobsCommandContainer{
 		ListAllQueues: mw.Traced(
 			di.TraceProvider, mw.Metric(
 				di.MeterProvider, mw.Logged(
-					di.Logger.(*slog.Logger), application.ListAllQueues(repo),
+					di.Logger.(*slog.Logger), application.ListAllQueues(adminContext.jobRepository),
 				),
 			),
 		),
 		GetQueue: mw.Traced(
 			di.TraceProvider, mw.Metric(
 				di.MeterProvider, mw.Logged(
-					di.Logger.(*slog.Logger), application.GetQueue(repo),
+					di.Logger.(*slog.Logger), application.GetQueue(adminContext.jobRepository),
 				),
 			),
 		),
 		GetWorkers: mw.Traced(
 			di.TraceProvider, mw.Metric(
 				di.MeterProvider, mw.Logged(
-					di.Logger.(*slog.Logger), application.GetWorkers(repo),
+					di.Logger.(*slog.Logger), application.GetWorkers(adminContext.jobRepository),
 				),
 			),
 		),
 		ScheduleJobs: mw.TracedU(
 			di.TraceProvider, mw.MetricU(
 				di.MeterProvider, mw.LoggedU(
-					di.Logger.(*slog.Logger), application.ScheduleJobs(models2.New(di.PGx)),
+					di.Logger.(*slog.Logger), application.ScheduleJobs(models.New(di.PGx)),
 				),
 			),
 		),
 		DeleteJob: mw.TracedU(
 			di.TraceProvider, mw.MetricU(
 				di.MeterProvider, mw.LoggedU(
-					di.Logger.(*slog.Logger), application.DeleteJob(repo),
+					di.Logger.(*slog.Logger), application.DeleteJob(adminContext.jobRepository),
 				),
 			),
 		),
 		RescheduleJob: mw.TracedU(
 			di.TraceProvider, mw.MetricU(
 				di.MeterProvider, mw.LoggedU(
-					di.Logger.(*slog.Logger), application.RescheduleJob(repo),
+					di.Logger.(*slog.Logger), application.RescheduleJob(adminContext.jobRepository),
 				),
 			),
 		),
 	}
+	jobsController.Cmds = container
+	jobsController.Queries = models.New(di.PGx)
+	adminContext.jobsController = jobsController
 
-	{
-		settingsCont := web.NewSettingsController(di.AdminRouter)
-		settingsCont.List()
-	}
-
-	cont := web.NewJobsController(di.Logger, repo, web2.NewDefaultPresenter(di.Settings), application.NewJobsApplication(di.PGx))
-	cont.Cmds = container
-	cont.Queries = models2.New(di.PGx)
-
-	{
-		jobs := di.AdminRouter.Group("/jobs")
-		jobs.GET("", cont.ListQueues())
-		jobs.GET("/", cont.ListQueues())
-		jobs.GET("/data/pending", cont.PendingJobsPieChartData())                // todo better htmx fruednly data URL
-		jobs.GET("/data/processed/:interval", cont.ProcessedJobsLineChartData()) // todo better htmx fruednly data URL
-		jobs.GET("/:queue", cont.ShowQueue())
-		jobs.GET("/:queue/delete/:job_id", cont.DeleteJob())
-		jobs.GET("/:queue/reschedule/:job_id", cont.RescheduleJob())
-		jobs.GET("/schedule", cont.CreateJobs()).Name = "admin.jobs.schedule"
-		jobs.POST("/schedule", cont.ScheduleJobs()).Name = "admin.jobs.new"
-		jobs.GET("/jobTypes", cont.ShowJobTypes())
-		jobs.GET("/payloads", cont.PayloadExamples())
-		jobs.GET("/workers", cont.ListWorkers())
-		jobs.GET("/maintenance", cont.ShowMaintenance()).Name = "admin.jobs.maintenance"
-		jobs.POST("/vacuum/:table", cont.VacuumJobTables())
-		jobs.POST("/history", cont.DeleteHistory())
-		jobs.POST("/history/prune", cont.PruneHistory())
-		jobs.GET("/history/size/", cont.EstimateHistorySize())
-		jobs.GET("/history/payload/size/", cont.EstimateHistoryPayloadSize())
-	}
-
-	adminContext := &AdminContext{}
-
-	jc := web.NewLogsController(di.Logger, di.Settings, models3.New(di.PGx), di.AdminRouter.Group("/logs"), web2.NewDefaultPresenter(di.Settings))
-	jc.ShowLogs()
-	jc.SettingLogs()
+	registerAdminRoutes(adminContext)
 
 	return adminContext, nil
 }
 
-type AdminContext struct{}
+type AdminContext struct {
+	*infrastructure.Container
+
+	jobRepository jobs.Repository
+
+	settingsController *web.SettingsController
+	jobsController     *web.JobsController
+	logsController     *web.LogsController
+}
 
 func (c *AdminContext) Shutdown(_ context.Context) error {
 	return nil
--- contexts/admin/internal/interfaces/web/settings.controller.go
+++ contexts/admin/internal/interfaces/web/settings.controller.go
@@ -1,12 +1,13 @@
 package web
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func NewSettingsController(routes *echo.Group) SettingsController {
-	return SettingsController{
+func NewSettingsController(routes *echo.Group) *SettingsController {
+	return &SettingsController{
 		r: routes,
 	}
 }
@@ -15,7 +16,7 @@ type SettingsController struct {
 	r *echo.Group
 }
 
-func (sc SettingsController) List() {
+func (sc *SettingsController) List() {
 	sc.r.GET("/settings", func(c echo.Context) error {
 		return c.String(http.StatusOK, "not implemented")
 	}).Name = "admin.settings"
